c8d6598be5c5ddbb006ec5652910c5466c943233
Anish Khale <khale.anish@gmail.com>
2018-11-01 17:44:10 -0400

    Add support for Slack IncomingWebhook
    - Posts Slack-flavored markdown to SLACK_WEBHOOK_URL

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const promisify = require('util').promisify;
 const appendFileAsync = promisify(fs.appendFile);
 const os = require('os');
+const { IncomingWebhook } = require('@slack/client');
 
 const prompts = [{
   name: 'confirmation',
@@ -27,23 +28,26 @@ const prompts = [{
 }];
 
 inquirer.prompt(prompts[0])
-  .then(answers => {
+  .then(answers => { // Ask questions on CLI
     if (!answers.confirmation) {
       throw new Error('confirmation');
     }
 
     return inquirer.prompt(prompts.slice(1));
   })
-  .then(answers => {
+  .then(answers => { // Format Github and Slack flavored Markdown
     let md = [];
     let answer = '';
     const today = (new Date()).toDateString();
+    let slackMessage = `*${today}*`;
+    slackMessage += '\n\n';
 
     md.push({
       h1: today
     });
 
     prompts.slice(1).forEach(prompt => {
+      // Github
       md.push({
         h2: prompt.message
       });
@@ -53,14 +57,37 @@ inquirer.prompt(prompts[0])
       md.push({
         ul: answer.filter(line => line.length)
       });
+
+      // Slack
+      slackMessage += `*${prompt.message}*`;
+      slackMessage += '```';
+      slackMessage += answer.map(a => `- ${a}`).join('\n');
+      slackMessage += '```';
+      slackMessage += '\n';
     });
 
-    return json2md.async(md);
+    return Promise.all([
+      json2md.async(md),
+      Promise.resolve(slackMessage)
+    ]);
   })
-  .then(markdown => {
+  .then(markdown => { // Save Github flavored Markdown
     const filename = (new Date()).toISOString().slice(0, 10);
 
-    return appendFileAsync(`${__dirname}/logs/${filename}.md`, markdown + os.EOL);
+    appendFileAsync(`${__dirname}/logs/${filename}.md`, markdown[0] + os.EOL);
+
+    return Promise.resolve(markdown[1]);
+  })
+  .then(slackMessage => { // Post to Slack
+    const url = process.env.SLACK_WEBHOOK_URL;
+
+    if (!url) {
+      return undefined;
+    }
+
+    const webhook = new IncomingWebhook(url);
+
+    return webhook.send(slackMessage);
   })
   .catch(err => {
     if (!/confirmation/.test(err)) {
